04e527382d8a58c55c1e942cf32890fbb04dc1b8
Erthium <ertyum35@gmail.com>
2024-11-13 07:40:44 +0300

    feat: create example env with basics and implement pydantic settings

---
+++ app/core/settings.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Settings(BaseSettings):
+  PROJECT_NAME: str = "Witch Puzzles Backend"
+  DESCRIPTION: str = "RestAPI of the Witch Puzzles Platform"
+  VERSION: str = "0.0.1"
+
+  DATABASE_URL: str = os.environ.get("DATABASE_URL")
+  DEVELOPMENT: bool = os.environ.get("DEVELOPMENT", 0) == 1
+  LOCK_DB_WRITE: bool = os.environ.get("LOCK_DB_WRITE", 0) == 1
+  PORT: int = os.environ.get("PORT", 4040)
+
+settings = Settings()
