900d3570fb267a9572ecb94eea5e92e267f5aec8
OAltintas <61349966+OAltintas@users.noreply.github.com>
2024-12-09 23:59:08 +0300

    feat: addressing to review comment.

--- app/schemes/sudoku_registry.py
+++ app/schemes/sudoku_registry.py
@@ -3,14 +3,14 @@ from app.core.database import Base
 from datetime import datetime
 
 class PuzzleRegistry(Base):
-  __tablename__ = "puzzle_registry"
+  __tablename__ = "sudoku_registry"
 
   id = Column(Integer, primary_key=True, index=True) 
   puzzle_id = Column(Integer)
   user_id = Column(Integer)
-  time_taken = Column(Integer, nullable=False) 
+  time_taken = Column(float, nullable=False) 
   is_valid_for_leaderboard = Column(bool, default=True)
-  created_at = Column(TIMESTAMP, default=datetime.now())
+  created_at = Column(datetime, default=datetime.now())
 
   __table_args__ = (
     UniqueConstraint('puzzle_id', 'user_id'),
--- app/schemes/user.py
+++ app/schemes/user.py
@@ -1,11 +1,12 @@
 from sqlalchemy import Column, Integer, String, Text, TIMESTAMP, UniqueConstraint
 from app.core.database import Base
 from datetime import datetime
+from sqlalchemy.dialects.postgresql import UUID
 
 class User(Base):
-  __tablename__ = "user"
+  __tablename__ = "users"
 
-  uid = Column(Integer, primary_key=True, index=True)
+  id = Column(UUID , primary_key=True, index=True)
   user_name = Column(Text, nullable=False)
   email = Column(Text, nullable=False, unique=True)
-  created_at = Column(TIMESTAMP, default=datetime.now())
+  created_at = Column(datetime, default=datetime.now())
