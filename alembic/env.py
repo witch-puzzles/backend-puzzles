a788a357dd0658bed4a836c436a7dc069192cf99
Erthium <ertyum35@gmail.com>
2024-12-27 01:58:38 +0300

    feat: add entities to alembic env

--- alembic/env.py
+++ alembic/env.py
@@ -10,6 +10,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from app.core.database import Base
+from app.entities import User, Sudoku, SudokuRegistry
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
