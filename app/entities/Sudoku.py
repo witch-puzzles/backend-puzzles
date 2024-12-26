cb4069defb416ad0c71fcf9ca766edca63a333f8
Oğuz Eren Kacar <kacar20@itu.edu.tr>
2024-12-27 00:40:10 +0300

    fix: fixed small issues in entity files.

--- app/entities/Sudoku.py
+++ app/entities/Sudoku.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, DateTime, UniqueConstraint, UUID
+from sqlalchemy import Column, String, DateTime, UniqueConstraint, UUID, Integer
 from app.core.database import Base
 from datetime import datetime
 
@@ -6,10 +6,10 @@ class Sudoku(Base):
   __tablename__ = "sudoku"
 
   id = Column(UUID, primary_key=True, index=True)
-  difficulty = Column(UUID, nullable=False)
+  difficulty = Column(Integer, nullable=False)
   puzzle_data = Column(String, nullable=False)
   created_at = Column(DateTime, default=datetime.now())
 
   __table_args__ = (
-    UniqueConstraint('puzzle_data', 'difficulty'),
+    UniqueConstraint('puzzle_data')
   )
--- app/entities/User.py
+++ app/entities/User.py
@@ -7,10 +7,8 @@ class User(Base):
 
   id = Column(UUID, primary_key=True, index=True)
   firebase_id = Column(String, nullable=False)
-  email = Column(String, nullable=False)
   created_at = Column(DateTime, default=datetime.now())
 
   __table_args__ = (
     UniqueConstraint('firebase_id'),
-    UniqueConstraint('email'),
   )
